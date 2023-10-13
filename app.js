6937081926f9c0c85d8eda68130afea061cfe7ce
pierre gomez <pierre222000@gmail.com>
2023-10-13 14:15:01 +0000

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const helloWord = require("./src/routes/HelloWorld/HelloWorld")
+const express = require('express')
+const error404 =require("./src/routes/404/404")
+const path = require('path');
+const app = express()
+const port = 3000 || process.env.PORT 
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/api/helloworld', helloWord)
+app.use('/*',error404)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
